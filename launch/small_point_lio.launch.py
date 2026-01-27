bf5dab15336eda2b758a72b9df01e27ab2afdeed
Ellipsis <213232180@seu.edu.cn>
2026-01-27 15:06:32 +0800

    修改参数

--- launch/small_point_lio.launch.py
+++ launch/small_point_lio.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
             "--y",
             "0.0",
             "--z",
-            "0.3",
+            "0.2",
             "--roll",
             "-0.785398", 
             "--pitch",
